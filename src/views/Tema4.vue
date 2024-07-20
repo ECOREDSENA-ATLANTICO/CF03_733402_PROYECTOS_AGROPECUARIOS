5ab266f4129f3edcc4e9e117452383747ad6cbd8
Liborio Castañeda Valencia <74385144+LiborioCastaneda99@users.noreply.github.com>
2024-07-20 02:54:29 -0500

    Terminación tema 4

---
+++ src/views/Tema4.vue
@@ -0,0 +1,169 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 4
+      h1 Estudio Ambiental
+      
+    .row.align-items-center.justify-content-center.mb-2
+      .col-lg-5.mb-3(data-aos="fade-right")
+        img.mt-3(src='@/assets/curso/images/tema4/img_1.png'  alt='')
+      .col-lg-7.mb-3(data-aos="fade-left")         
+        .cajon.bg-10.p-4.ps-5.bg_18.mb-5.mb-lg-3
+          p.mb-0 En el estudio ambiental se deben tener en cuenta además de la legislación que es obligatoria para la producción de los productos agropecuarios, la normatividad que aplica al cuidado de los recursos naturales presenten el área del proyecto, el tratamiento o disposición de los residuos propios del proceso productivo y desechos provenientes de empaques de agroquímicos, de medicamentos veterinarios o empaques sobrantes.
+        p.mb-3 Si el proyecto está destinado para la comercialización en el exterior y va a realizar la certificación en alguna norma como global GAP, debe conocer la reglamentación, exigencias y adecuaciones en el producto y al entorno del área de producción. La finalidad de este estudio es reducir al máximo los impactos negativos sobre el ambiente y crear planes de acción que conlleven implementar acciones tendientes a realizar procesos productivos cada vez más amigables con el medio ambiente.
+
+    Separador
+
+    #t_4_1.titulo-segundo
+      .h2(data-aos="fade-right") 4.1 Legislación ambiental
+
+    .row.align-items-center.justify-content-center.mb-2
+      .col-lg-7.mb-5(data-aos="fade-left")         
+        .cajon.color-acento-contenido.p-4.ps-5.bg_18.mb-5.mb-lg-3
+          p.mb-0 La legislación en Colombia es amplia teniendo en cuenta que regula el uso, sanciones, cuidado y delimitación de los recursos naturales presentes en el territorio nacional, las explotaciones agropecuarias no están exentas de la regulación y control en su uso como de explotación; cada productor es responsable del uso que le dé a los recursos naturales que se encuentran en su predio y debe conocer la normatividad reguladora. Esta normatividad es para el recurso suelo, uso, tenencia y aprovechamiento de las aguas, disposición de residuos sólidos, líquidos y material particulado que se genere del proceso productivo.
+        p.mb-3 Es importante recordar que en la Constitución Política de Colombia se encuentran las normas y principios ambientales generales, algunos de ellos son:
+
+      .col-lg-5.mb-5(data-aos="fade-right")
+        img.mt-3(src='@/assets/curso/images/tema4/img_2.png'  alt='')
+
+
+      .col-md-6.mb-3
+        .tarjeta.bg-03.p-2
+          .row.justify-content-around.align-items-center
+            .col-auto
+              img(src="@/assets/curso/images/tema4/img_3.svg")
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  p.mb-0 <b>Artículo 7. </b>Hace reconocimiento expreso de la pluralidad étnica y cultural de la Nación, así como del deber del Estado para con su protección.
+
+      .col-md-6.mb-3
+        .tarjeta.bg-06.p-2
+          .row.justify-content-around.align-items-center
+            .col-auto
+              img(src="@/assets/curso/images/tema4/img_3.svg")
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  p.mb-0 <b>Artículo 8. </b>Establece la obligación del Estado y de las personas para con la conservación de las riquezas naturales y culturales de la Nación.
+      
+      .col-md-6.mb-3
+        .tarjeta.bg-05.p-2
+          .row.justify-content-around.align-items-center
+            .col-auto
+              img(src="@/assets/curso/images/tema4/img_3.svg")
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  p.mb-0 <b>Artículo 49. </b>Consagra como servicio público la atención de la salud y el saneamiento ambiental y ordena al Estado la organización, dirección y reglamentación de los mismos.
+      
+      .col-md-6.mb-3
+        .tarjeta.bg-03.p-2
+          .row.justify-content-around.align-items-center
+            .col-auto
+              img(src="@/assets/curso/images/tema4/img_3.svg")
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  p.mb-0 <b>Artículo 58. </b>Establece que la propiedad es una función social que implica obligaciones y que como tal le es inherente una función ecológica.
+      
+      .col-md-6.mb-3
+        .tarjeta.bg-06.p-2
+          .row.justify-content-around.align-items-center
+            .col-auto
+              img(src="@/assets/curso/images/tema4/img_3.svg")
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  p.mb-0 <b>Artículo 63. </b>Determina que los bienes de uso público, los parques naturales, las tierras comunales de grupos étnicos y los demás bienes que determine la ley son inalienables, imprescriptibles e inembargables.
+      
+      .col-md-6.mb-3
+        .tarjeta.bg-05.p-2
+          .row.justify-content-around.align-items-center
+            .col-auto
+              img(src="@/assets/curso/images/tema4/img_3.svg")
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  p.mb-0 <b>Artículo 79. </b>Consagra el derecho de todas las personas residentes en el país de gozar de un ambiente sano.
+      
+      .col-md-6.mb-5
+        .tarjeta.bg-03.p-2
+          .row.justify-content-around.align-items-center
+            .col-auto
+              img(src="@/assets/curso/images/tema4/img_3.svg")
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  p.mb-0 <b>Artículo 80. </b>Establece como deber del Estado la planificación del manejo y aprovechamiento de los recursos naturales, para garantizar su desarrollo sostenible, su conservación, restauración o sustitución.
+      
+      .col-md-6.mb-5
+        .tarjeta.bg-06.p-2
+          .row.justify-content-around.align-items-center
+            .col-auto
+              img(src="@/assets/curso/images/tema4/img_3.svg")
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  p.mb-0 <b>Artículo 88. </b>Consagra acciones populares para la protección de derechos e intereses colectivos sobre el medio ambiente, entre otros, bajo la regulación de la ley.
+      
+      .col-md-6.mb-5
+        .tarjeta.bg-05.p-2
+          .row.justify-content-around.align-items-center
+            .col-auto
+              img(src="@/assets/curso/images/tema4/img_3.svg")
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  p.mb-0 <b>Artículo 95. </b>Establece como deber de las personas, la protección de los recursos culturales y naturales del país, así como de velar por la conservación de un ambiente sano.
+      
+      .col-md-6.mb-5
+        .tarjeta.bg-03.p-2
+          .row.justify-content-around.align-items-center
+            .col-auto
+              img(src="@/assets/curso/images/tema4/img_3.svg")
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  p.mb-0 <b>Artículo 330. </b>Establece la administración autónoma de los territorios indígenas con ámbitos de aplicación en los usos del suelo y la preservación de los recursos naturales, entre otros. UPME, 2021.
+
+      .col-lg-10.mb-5
+        .tarjeta.color-primario.p-3
+          .row.justify-content-around.align-items-center.p-3
+            .col-auto
+              img(src="@/assets/curso/images/tema4/img_4.svg").img65
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  h4.mb-1 Recurso web
+                  p.mb-0 Normatividad ambiental y sanitaria. Unidad de Planeación Minero Energética (UPME)
+                .col-sm-auto
+                  a.boton.color-acento-botones.text-white(href="https://docs.google.com/document/d/1PPRatnr8cI2Hxkv8hzg_e3qw3N0FVQs3yvO0rZLNaJE/edit?usp=sharing" target="_blank")
+                    span Ver sitio
+                    i.fas.fa-file-pdf
+      
+  
+</template>
+
+<script>
+export default {
+  name: 'Tema4',
+  components: {},
+  data: () => ({
+    // variables de vue
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+  updated() {
+    this.$aosRefresh()
+  },
+}
+</script>
+
+<style lang="sass"></style>
